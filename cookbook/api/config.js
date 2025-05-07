67763cd24e7b4e1427fe00f936e65a1f2dfb0e90
Newbluewood <nebojsa.simovic@outlook.com>
2025-05-07 12:22:52 +0200

    api config changed https

--- cookbook/api/config.js
+++ cookbook/api/config.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
-const instance = axios.create({ baseURL: 'http://483r123.e2.mars-hosting.com/cookbook/api/' })
+const instance = axios.create({ baseURL: 'https://483r123.e2.mars-hosting.com/cookbook/api/' })
 
 export default instance
